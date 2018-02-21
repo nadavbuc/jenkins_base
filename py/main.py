8423544712b38d7552313bbc5b8af40c5527c701
nadav <nadavbuc@gmail.com>
2018-02-21 22:41:46 +0200

    Mainfile V1

--- py/main.py
+++ py/main.py
@@ -3,6 +3,8 @@ if sys.argv[1] <= '2':
     if sys.argv[1] == '2':
         print ("Jenkins job name: " + str(sys.argv[2]))
     else:
+        print (sys.argv[1])
+        print (sys.argv[2])
         abc
 elif sys.argv[1] > '3':
     if (int(sys.argv[1]) + 1) % '3' == '0':
